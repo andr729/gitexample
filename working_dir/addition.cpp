115e27a4665896d55d8e0e6d4dcb4480fc3d4ebf
andrzej <andrzej@rpi.pl>
2021-11-23 17:42:59 +0100

    Add: sum function

--- working_dir/addition.cpp
+++ working_dir/addition.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "addition.hpp"
 
+int sum(int a, int b) {
+    return a + b;
+}
+
 int add(int a, int b) {
     std::cerr << "log: add(" << a << ", " << b << ")\n";
     return a + b;
