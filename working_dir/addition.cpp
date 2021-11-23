5f9f5e9ba5abcfa64970f2d6835f3837744b9168
andrzej <andrzej@rpi.pl>
2021-11-23 17:22:15 +0100

    stranger algorithm

--- working_dir/addition.cpp
+++ working_dir/addition.cpp
@@ -2,5 +2,14 @@
 #include "addition.hpp"
 
 int add(int a, int b) {
-    return a + b;
+    int result = 0;
+
+    for(int i = 0; i < 32; i++) {
+        int pa = a >> i;
+        int pb = b >> i;
+        if (pa % 2 == 1) result += 1 << i;
+        if (pb % 2 == 1) result += 1 << i;
+    }
+
+    return result;
 }
