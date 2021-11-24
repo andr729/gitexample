5878d14288b2bb41e24580529f7221c163e1f860
andrzej <andrzej@rpi.pl>
2021-11-24 10:03:08 +0100

    some docs

--- working_dir/addition.cpp
+++ working_dir/addition.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "addition.hpp"
 
+/**
+ * @brief Adds two number 
+ * @param a first integer
+ * @param b second integer
+ * @return int : a + b
+ */
 int add(int a, int b) {
     return a + b;
 }
