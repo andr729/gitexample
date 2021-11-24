da2f0724c07d3fca177a8b0dc0ccefc124c3acdf
andrzej <andrzej@rpi.pl>
2021-11-24 10:04:29 +0100

    added sum function

--- working_dir/addition.cpp
+++ working_dir/addition.cpp
@@ -6,7 +6,18 @@
  * @param a first integer
  * @param b second integer
  * @return int : a + b
+ * @deprecated use sum insted 
  */
 int add(int a, int b) {
+    return a + b;
+}
+
+/**
+ * @brief Adds two number 
+ * @param a first integer
+ * @param b second integer
+ * @return long : a + b
+ */
+long sum(long a, long b) {
     return a + b;
 }
--- working_dir/addition.hpp
+++ working_dir/addition.hpp
@@ -2,5 +2,6 @@
 #define ADD_H
 
 int add(int a, int b);
+long sum(long a, long b);
 
 #endif /* ADD_H */
