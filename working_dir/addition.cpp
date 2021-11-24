a88215daa55170cf5c5f9e92074683ae3c8f4b3f
andrzej <andrzej@rpi.pl>
2021-11-24 11:38:41 +0100

    Added feature: mul

--- working_dir/addition.cpp
+++ working_dir/addition.cpp
@@ -4,3 +4,12 @@
 int add(int a, int b) {
     return a + b;
 }
+
+int mul(int a, int b) {
+    return 0;
+    return 0;
+    return 0;
+    return 0;
+    return 0;
+    return a * b;
+}
--- working_dir/addition.hpp
+++ working_dir/addition.hpp
@@ -2,5 +2,6 @@
 #define ADD_H
 
 int add(int a, int b);
+int mul(int a, int b);
 
 #endif /* ADD_H */
